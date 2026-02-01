3f5a2d92730012eca295a0e5833f8dab056dc1af
Mohit Sharma <msharma89339@gmail.com>
2026-02-01 22:06:36 +0530

    update model nd controller

--- Models/formModel.js
+++ Models/formModel.js
@@ -4,17 +4,21 @@ const clientSchema = new mongoose.Schema(
   {
     clientName: { type: String, required: true },
     fatherName: { type: String, required: true },
+    fatherPhone: { type: String, required: true },
     gender: { type: String, required: true },
-    dob: { type: String , required: true },
+    dob: { type: Date, required: false },
     age: { type: Number, required: true },
     phone: { type: String, required: true },
-    email: { type: String, required: true  },
+    email: { type: String, required: true },
     address: { type: String, required: true },
     Nationality: { type: String, required: true },
-    familyMembers: { type: Number, required: true  },
-
+    relationship: { 
+      type: String, 
+      required: true,
+      enum: ["Married", "Unmarried"]
+    },
+    occupation: { type: String, required: false },
     photo: { type: String, required: true },
-    biometric: { type: String, required: false },
   },
   { timestamps: true },
 );
